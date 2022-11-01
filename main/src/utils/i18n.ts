1e31fc071554c8f1b50684157202aa2b22c6556a
lqs <957698457@qq.com>
2022-11-01 11:21:58 +0800

    i18n any

--- main/src/utils/i18n.ts
+++ main/src/utils/i18n.ts
@@ -2,8 +2,6 @@
  * 国际化 utils
  * @author LiQingSong
  */
-import { LocaleMessages } from '@intlify/core-base';
-import { VueMessageType } from "vue-i18n";
 
 // window.localStorage 存储key
 export const localeKey = 'locale';
@@ -39,8 +37,8 @@ export const setHtmlLang = (lang: string): void => {
  * @returns string
  * @author LiQingSong
  */
-export const getLocale = (): string => {   
-    const lang = typeof window.localStorage !== 'undefined' ? window.localStorage.getItem(localeKey) : '';    
+export const getLocale = (): string => {
+    const lang = typeof window.localStorage !== 'undefined' ? window.localStorage.getItem(localeKey) : '';
     const isNavigatorLanguageValid = typeof navigator !== 'undefined' && typeof navigator.language === 'string';
     const browserLang = isNavigatorLanguageValid ? navigator.language.split('-').join('-') : '';
     return lang || browserLang || defaultLang;
@@ -53,7 +51,7 @@ export const getLocale = (): string => {
  * @author LiQingSong
  */
 export const setLocale = (lang: string, realReload = true, callback: () => void ): void => {
-  
+
   if (lang !== undefined && !localeNameExp(lang)) {
     // for reset when lang === undefined
     throw new Error('setLocale lang format error');
@@ -72,7 +70,7 @@ export const setLocale = (lang: string, realReload = true, callback: () => void 
             callback();
         }
     }
-    
+
   }
 };
 
@@ -80,47 +78,47 @@ export const setLocale = (lang: string, realReload = true, callback: () => void 
  * 自动导入 框架自定义语言
  * @author LiQingSong
  */
-export function importAllLocales(): LocaleMessages<VueMessageType> {
-    const modules: LocaleMessages<VueMessageType> = {};
+export function importAllLocales(): any {
+    const modules: any = {};
     try {
         // 导入 @/views 下文件，包含子目录，文件名为：[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts
-        const viewsRequireContext: __WebpackModuleApi.RequireContext = require.context('../views', true, /[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts$/); 
+        const viewsRequireContext: __WebpackModuleApi.RequireContext = require.context('../views', true, /[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts$/);
         viewsRequireContext.keys().forEach(fileName => {
             // 获取内容
             const modulesConent = viewsRequireContext(fileName);
-            if(modulesConent.default) {               
+            if(modulesConent.default) {
                 // 获取 PascalCase 命名
                 const modulesName = fileName.replace(/(.*\/)*([^.]+).*/ig,"$2");
-                
+
                 if(modules[modulesName]) {
                     modules[modulesName] = {
                         ...modules[modulesName],
                         ...modulesConent.default
                     }
                 } else {
-                    modules[modulesName] = modulesConent.default; 
+                    modules[modulesName] = modulesConent.default;
                 }
 
             }
 
         });
-        
+
         // 导入 @/layouts 下文件，包含子目录，文件名为：[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts
-        const layoutsRequireContext: __WebpackModuleApi.RequireContext = require.context('../layouts', true, /[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts$/); 
+        const layoutsRequireContext: __WebpackModuleApi.RequireContext = require.context('../layouts', true, /[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts$/);
         layoutsRequireContext.keys().forEach(fileName => {
             // 获取内容
             const modulesConent = layoutsRequireContext(fileName);
-            if(modulesConent.default) {               
+            if(modulesConent.default) {
                 // 获取 PascalCase 命名
                 const modulesName = fileName.replace(/(.*\/)*([^.]+).*/ig,"$2");
-                
+
                 if(modules[modulesName]) {
                     modules[modulesName] = {
                         ...modules[modulesName],
                         ...modulesConent.default
                     }
                 } else {
-                    modules[modulesName] = modulesConent.default; 
+                    modules[modulesName] = modulesConent.default;
                 }
 
             }
@@ -128,21 +126,21 @@ export function importAllLocales(): LocaleMessages<VueMessageType> {
         });
 
         // 导入 @/components 下文件，包含子目录，文件名为：[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts
-        const componentsRequireContext: __WebpackModuleApi.RequireContext = require.context('../components', true, /[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts$/); 
+        const componentsRequireContext: __WebpackModuleApi.RequireContext = require.context('../components', true, /[/\\]locales[/\\]([a-z]{2})-?([A-Z]{2})?\.ts$/);
         componentsRequireContext.keys().forEach(fileName => {
             // 获取内容
             const modulesConent = componentsRequireContext(fileName);
-            if(modulesConent.default) {               
+            if(modulesConent.default) {
                 // 获取 PascalCase 命名
                 const modulesName = fileName.replace(/(.*\/)*([^.]+).*/ig,"$2");
-                
+
                 if(modules[modulesName]) {
                     modules[modulesName] = {
                         ...modules[modulesName],
                         ...modulesConent.default
                     }
                 } else {
-                    modules[modulesName] = modulesConent.default; 
+                    modules[modulesName] = modulesConent.default;
                 }
 
             }
@@ -150,11 +148,11 @@ export function importAllLocales(): LocaleMessages<VueMessageType> {
         });
 
         // 导入 @/locales 下文件，不包含子目录，文件名为：([a-z]{2})-?([A-Z]{2})?\.ts
-        const localesRequireContext: __WebpackModuleApi.RequireContext = require.context('../locales', false, /([a-z]{2})-?([A-Z]{2})?\.ts$/); 
+        const localesRequireContext: __WebpackModuleApi.RequireContext = require.context('../locales', false, /([a-z]{2})-?([A-Z]{2})?\.ts$/);
         localesRequireContext.keys().forEach(fileName => {
             // 获取内容
             const modulesConent = localesRequireContext(fileName);
-            if(modulesConent.default) {               
+            if(modulesConent.default) {
                 // 获取 PascalCase 命名
                 const modulesName = fileName.replace(/(.*\/)*([^.]+).*/ig,"$2");
                 if(modules[modulesName]) {
@@ -163,14 +161,14 @@ export function importAllLocales(): LocaleMessages<VueMessageType> {
                         ...modulesConent.default
                     }
                 } else {
-                    modules[modulesName] = modulesConent.default; 
+                    modules[modulesName] = modulesConent.default;
                 }
 
             }
 
         });
-        
-        
+
+
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(error);
